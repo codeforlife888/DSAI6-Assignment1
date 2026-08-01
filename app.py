d62e8f72c990de94856a38fe8ef2c2472ade1cec
leonardlai <lailisheng@outlook.sg>
2026-08-01 23:29:14 +0800

    added page navigations, updated syntax to fit new dataset

--- app.py
+++ app.py
@@ -4,23 +4,51 @@ import streamlit as st
 
 st.set_page_config(page_title="Singapore Job Market Dashboard", layout="wide")
 
-DATA_PATH = Path(__file__).resolve().parent / "sample_jobs.csv"
-df = pd.read_csv(
-    DATA_PATH,
-    parse_dates=[
-        "metadata_newPostingDate",
-        "metadata_originalPostingDate",
-        "metadata_expiryDate",
-    ],
-)
+
+DATA_PATH = Path(__file__).resolve().parent / "output3.csv"
+
+@st.cache_data
+def load_data():
+    return pd.read_csv(
+        DATA_PATH,
+        parse_dates=[
+            "New_Post_Date",
+            "Orig_Post_Date",
+            "Expiry_Date",
+        ],
+        sep=";"
+    )
 
 st.title("Singapore Job Market Dashboard")
 
+# Navigation
+pg = st.navigation(
+    [
+        st.Page(
+            "pages/overview.py",
+            title="Overview",
+            icon=":material/dashboard:",
+            default=True,
+        ),
+        st.Page(
+            "pages/use_case_1.py", 
+            title="Business Case 1", 
+            icon=":material/menu_book:"
+        ),
+        st.Page(
+            "pages/use_case_2.py", 
+            title="Business Case 2", 
+            icon=":material/code:"
+        ),
+    ]
+)
+#pg.run()
+
+
 with st.sidebar:
     st.header("Filters")
-    category_options = sorted(df["primary_category"].dropna().unique())
+    df = load_data()
+    category_options = sorted(df["Category"].dropna().unique())
     selected_categories = st.multiselect(
         "Category", category_options, default=category_options
     )
-
-
