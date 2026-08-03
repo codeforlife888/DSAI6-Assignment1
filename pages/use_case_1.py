929fc23ab2c0ca44e0c7d4ec2558e489db5a77ac
Boon Wee <bwong_sg@yahoo.com>
2026-08-03 22:35:43 +0800

    CHORE: update use case 1

--- pages/use_case_1.py
+++ pages/use_case_1.py
@@ -1,1 +1,34 @@
-# All Content/ Filters / Charts for Use Case 1 goes here :
+from pathlib import Path
+import streamlit as st
+import pandas as pd1
+import plotly.express as px
+from plotly.subplots import make_subplots
+from config  import load_data
+
+st.markdown(
+    "**Business Case 1: Top roles by vacancies**")
+
+st.markdown(
+    "Which are the top 10 categories especially IT?")
+
+df = load_data()
+filter_df = df.groupby("Category").agg({"Vacancies": "sum"}).reset_index()
+
+# Create bar chart
+fig = px.bar(
+    filter_df,
+    x="Category",   # categories on x-axis
+    y="Vacancies",      # number of vacancies on y-axis
+    title="Job Vacancies by Category",
+    text="Vacancies"    # show vacancy numbers on bars
+)
+
+# Improve layout
+fig.update_layout(
+    xaxis_title="Job Category",
+    yaxis_title="Number of Vacancies",
+    template="plotly_white"
+)
+
+# Show the figure
+st.plotly_chart(fig)
